c17c57fcd8c3f18c1fa9c6e31670d126c51b1d72
Akmal Yunusov <akmal.yunusov13@gmail.com>
2022-07-12 03:33:07 +0300

    похожее хоть на что-то

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,45 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-        return MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2.0,
-                    color: Color(0xFF000000),
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            // margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
                   ),
-                  borderRadius: BorderRadius.circular(10)
+                ],
+                border: Border.all(
+                  width: 0.5,
+                  color: Color(0xFFFFFFFF),
                 ),
-              ),
+                borderRadius: BorderRadius.circular(15)),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Tasks",
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
