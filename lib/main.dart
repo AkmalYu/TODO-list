8c41928a5461d139a3623bc96ddbff889d0b7198
Akmal Yunusov <akmal.yunusov13@gmail.com>
2022-07-12 09:06:15 +0300

    не доработанная автоматическая смена темы

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
+ThemeData _darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.black,
+
+);
+
+ThemeData _lightTheme = ThemeData(
+
+    brightness: Brightness.light,
+    primaryColor: Colors.white,
+
+);
+
+bool _light = true;
+
 void main() => runApp(MyApp());
+
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: _light ? _lightTheme : _darkTheme,
+      darkTheme: _darkTheme,
       home: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(20),
@@ -18,7 +37,7 @@ class MyApp extends StatelessWidget {
                     color: Colors.grey.withOpacity(0.5),
                     spreadRadius: 5,
                     blurRadius: 5,
-                    offset: Offset(0, 3), // changes position of shadow
+                    offset: Offset(0, 3), // changes pogsition of shadow
                   ),
                 ],
                 border: Border.all(
