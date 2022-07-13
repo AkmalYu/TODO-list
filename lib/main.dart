ca73b40e9a40356cd98c6cb67719c1260fb9311c
Akmal Yunusov <akmal.yunusov13@gmail.com>
2022-07-13 13:13:53 +0300

    кнопка сменны темы

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,99 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-
-ThemeData _darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primaryColor: const Color(0xFF1E1F25),
-);
-
-ThemeData _lightTheme = ThemeData(
-  brightness: Brightness.light,
-  primaryColor: Colors.white,
-);
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final ValueNotifier<ThemeMode> _notifier = ValueNotifier(ThemeMode.light);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: _lightTheme,
-      darkTheme: _darkTheme,
-      themeMode: ThemeMode.system,
-      home: Scaffold(
-        body: Padding(
-          padding:
-              const EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 20),
-          // отступы за пределами контейнера
-          child: Container(
-            padding: EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 20),
-            //отступы внутри контейнера
-            decoration: BoxDecoration(
-                color: Color(0xFF1E1F25),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.3),
-                    spreadRadius: 2,
-                    blurRadius: 4,
-                    offset: const Offset(0, 0), // changes pogsition of shadow
-                  ),
-                ],
-                border: Border.all(
-                  width: 0.1,
-                  color: Color(0xFFEBEBEB),
-                ),
-                borderRadius: BorderRadius.circular(15)),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Tasks",
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: _notifier,
+      builder: (_, mode, __) {
+        return MaterialApp(
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode: mode,
+          home: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.only(
+                  top: 40, left: 20, right: 20, bottom: 20),
+              // отступы за пределами контейнера
+              child: Container(
+                padding:
+                EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 20),
+                //отступы внутри контейнера
+                decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 4,
+                        offset:
+                        const Offset(0, 0), // changes pogsition of shadow
                       ),
+                    ],
+                    border: Border.all(
+                      width: 0.1,
+                      color: Color(0xFFEBEBEB),
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Color(0xFFFFFFF),
-                        // padding: EdgeInsets.only(left: 5, right: 1),
-                        fixedSize: Size(30, 30),
-                      ),
-                      child: Icon(
-                        CupertinoIcons.add,
-                        size: 40,
-                      ),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Tasks",
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        ButtonTheme(
+                          minWidth: 20,
+                          height: 40,
+                          child: MaterialButton(
+                            onPressed: () => _notifier.value =
+                            mode == ThemeMode.light
+                                ? ThemeMode.dark
+                                : ThemeMode.light,
+                            child: Icon(
+                              CupertinoIcons.eye,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                        ButtonTheme(
+                          minWidth: 15,
+                          height: 40,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            color: Color(0xFFFFFFF),
+
+                            child: Icon(
+                              CupertinoIcons.add,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      height: 30,
+                      color: Color(0xFFEBEBEB),
+                      thickness: 1,
                     ),
                   ],
                 ),
-                Divider(
-                  height: 30,
-                  color: Color(0xFFEBEBEB),
-                  thickness: 1,
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
